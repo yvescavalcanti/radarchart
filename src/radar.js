5ce1bc1656a000afcb8791325b393c6879dd681d
Yves <yvescavalcanti@gmail.com>
2018-08-19 21:47:20 -0300

    190818

--- src/radar.js
+++ src/radar.js
@@ -1,20 +1,36 @@
 function Radar(){
 	var axes = null;
 	var center = null;
+	var dados = null;	
+	var margin = {left:20, top:20};
+	var color = d3.schemeCategory10;
 	function main(selection){
 
 		selection.each(function(){
+			
 			var el = d3.select(this);
 			var svg = d3.select('#chart').append('svg').attr('width',400)
 			.attr('height',400);
 
-			var g = svg.append('g');
+			var g = svg.append('g').attr('transform','translate(20,20)');
 
 			g.selectAll('.axes').data(axes, function(d){return d.id;}).enter().append('line')
 			.attr('x1',center.x).attr('y1',center.y)
 			.attr('x2',function(d){return d.apex.x;})
 			.attr('y2',function(d){return d.apex.y;})
 			.attr('fill','none').attr('stroke-width','1px').attr('stroke','blue');
+			var enterData = g.selectAll('.entity').data(dados).enter();
+			enterData.append('polygon').attr('fill',function(d,i){
+					return color[i];
+				}).attr('stroke','green').attr('class','entity')
+				.attr("points", function(d){
+						console.log("d:");
+						console.log(d);
+						var points = axes.getPoints(d);
+						console.log(points);
+						return points.map(function(p){return [p.x, p.y].join(",");}).join(" ");
+			});
+			//enterData.append('circle').attr('fill','red').attr(	
 
 		});
 		
@@ -27,6 +43,10 @@ function Radar(){
 	main.center = function(_){
 		return arguments.length ? (center=_, main) : center;
 	}
+
+	main.data = function(_){
+		return arguments.length ? (dados=_, main) : dados;
+	};
 
 	return main;
 }
--- src/radial-axes.js
+++ src/radial-axes.js
@@ -1,3 +1,19 @@
+
+
+function polarToCartesian(radio, angle, scale){
+	var _x = Math.cos(radians(angle)); 
+	var _y = Math.sin(radians(angle));
+	//console.log("_x:"+_x);
+	//console.log("_y:"+_y);
+	var f = function(val){
+		return { 
+			x: radio + scale(val)*_x, 
+			y: radio - scale(val)*_y 
+		}; 
+	};
+	return f;
+}
+
 /* converte graus em radianos */
 function radians(angle){
 	return angle * (Math.PI / 180);
@@ -15,6 +31,30 @@ function _y(radio){
 	};	
 }
 
+
+function getAxesMax(key,data)
+{
+	return d3.max(
+		data.map(
+			function(d)
+			{
+				return d[key];
+			}
+		)
+	);
+}
+function getAxesMin(key,data)
+{
+	return d3.min(
+		data.map(
+			function(d)
+			{
+				return d[key];
+			}
+		)
+	);
+}
+
 /* Função que retorna função geradora de eixos radiais */
 function radialAxes(){
 	var keys = [];
@@ -22,24 +62,36 @@ function radialAxes(){
 	var step = 0;
 	var x,y;
 	var axes = [];
-
+	
 	function main(data){
 		// defining angle between axes
 		step = 360 / keys.length;
+		console.log(data);
 		// start angle for the first axes in degrees
 		var aux = 90;
-	
 		var axes = [];
 		keys.forEach(function(k,i){
+			var scale = d3.scaleLinear().range([0,radio])
+				.domain([0 ,getAxesMax(k,data)]);
 			axes.push({
+				// id para identificação
 				id:i,
+				key:k,
+				// ângulo do eixo
 				angle: aux,
+				// localização do extremo do eixo
 				apex:{
 					x:x(radio, aux),
 					y:y(radio, aux)
-				}
+				},
+				// função para posicionar pontos ao longo do eixo
+				getPoint:function(d)
+					{
+						console.log("aux:"+aux);
+						return (polarToCartesian(radio,this.angle,scale))(d[k]);
+					}
 			});
-
+			// calcula ângulo do próximo eixo
 			aux+=step;
 			
 		});
@@ -49,7 +101,19 @@ function radialAxes(){
 				return a.apex;	
 			});	
 		};
-
+		
+		axes.getPoints = function(d){
+		var p = [];
+		axes.forEach(function(a){
+			p.push(
+				a.getPoint(d)
+			);
+		});
+		return p;
+		
+	}
+	
+	
 		return axes;
 	}
 
